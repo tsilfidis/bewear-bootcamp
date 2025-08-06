c942cb4a4da40c8c6e9ff3ad7fb6e70a1e58e1ac
unknown <tsilfidis@gmail.com>
2025-08-06 17:46:58 -0400

    chore: correct imports from components ESLint

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -5,7 +5,7 @@ import SingUpForm from "./components/sing-up-form";
 
 const Authentication = () => {
   return (
-    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
+    <div className="flex w-full flex-col items-center justify-center gap-6 p-5">
       <Tabs defaultValue="sing-in">
         <TabsList>
           <TabsTrigger value="sing-in">Entrar</TabsTrigger>
