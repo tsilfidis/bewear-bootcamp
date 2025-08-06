fc2e393bd5a555ce5f4176ea3f70b435fe1a9787
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2025-08-06 11:44:32 -0400

    feat: add sing in and sign up form components

---
+++ src/app/authentication/components/sing-up-form.tsx
@@ -0,0 +1,134 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z
+  .object({
+    name: z.string().trim().min(1, "Digite um nome válido."),
+    email: z.email("Email inválido."),
+    password: z.string().min(8, "Digite uma senha válida."),
+    passwordConfirmation: z.string().min(8, "Digite uma senha válida."),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: "As senhas não coincidem.",
+    path: ["passwordConfirmation"],
+  });
+
+type FormValues = z.infer<typeof formSchema>;
+
+const SingUpForm = () => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+  });
+  function onSubmit(values: FormValues) {
+    console.log("FORMULARIO VALIDO E ENVIADO");
+    console.log(values);
+  }
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Criar conta</CardTitle>
+          <CardDescription>Crie uma conta para continuar</CardDescription>
+        </CardHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <CardContent className="grid gap-6">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu nome" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Digite sua senha'"
+                        {...field}
+                        type="password"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="passwordConfirmation"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirmar senha</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Digite sua senha novamente"
+                        {...field}
+                        type="password"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button type="submit">Criar conta</Button>
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
+    </>
+  );
+};
+
+export default SingUpForm;
---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,25 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import SignInForm from "./components/sign-in-form";
+import SingUpForm from "./components/sing-up-form";
+
+const Authentication = () => {
+  return (
+    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
+      <Tabs defaultValue="sing-in">
+        <TabsList>
+          <TabsTrigger value="sing-in">Entrar</TabsTrigger>
+          <TabsTrigger value="sing-up">Criar conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="sing-in">
+          <SignInForm />
+        </TabsContent>
+        <TabsContent value="sing-up">
+          <SingUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Authentication;
