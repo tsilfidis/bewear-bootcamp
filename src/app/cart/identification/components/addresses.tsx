2ff043e2220ceb794c66e01b848246cd69accbab
Panagitis Alexandro Tsilfidis <tsilfidis@gmail.com>
2025-11-27 11:51:37 -0400

    feat: add address form

--- src/app/cart/identification/components/addresses.tsx
+++ src/app/cart/identification/components/addresses.tsx
@@ -1,12 +1,62 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { PatternFormat } from "react-number-format";
+import z from "zod";
 
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
+const addressFormSchema = z.object({
+  email: z.email("Digite um e-mail válido"),
+  name: z.string().min(1, "O nome completo é obrigatório"),
+  cpf: z.string().min(11, "O CPF é obrigatório"),
+  phone: z.string().min(1, "O celular é obrigatório"),
+  zipCode: z.string().min(8, "O CEP é obrigatório"),
+  address: z.string().min(1, "O endereço é obrigatório"),
+  number: z.string().min(1, "O número é obrigatório"),
+  complement: z.string().optional(),
+  neighborhood: z.string().min(1, "O bairro é obrigatório"),
+  city: z.string().min(1, "A cidade é obrigatória"),
+  state: z.string().min(1, "O estado é obrigatório"),
+});
+
+type AddressFormValues = z.infer<typeof addressFormSchema>;
+
 const Addresses = () => {
+  const form = useForm<AddressFormValues>({
+    resolver: zodResolver(addressFormSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      cpf: "",
+      phone: "",
+      zipCode: "",
+      address: "",
+      number: "",
+      complement: "",
+      neighborhood: "",
+      city: "",
+      state: "",
+    },
+  });
+  const onSubmit = (data: AddressFormValues) => {
+    console.log(data);
+    // Lógica de submissão aqui
+  };
   const [selectedAdress, setSelectedAdress] = useState<string | null>(null);
   return (
     <Card>
@@ -24,7 +74,204 @@ const Addresses = () => {
             </CardContent>
           </Card>
         </RadioGroup>
-        {selectedAdress === "add_new" && }
+        {selectedAdress === "add_new" && (
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-4 pt-4"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>E-mail</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu e-mail" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome completo</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Digite seu nome completo"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                <FormField
+                  control={form.control}
+                  name="cpf"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>CPF</FormLabel>
+                      <FormControl>
+                        <PatternFormat
+                          format="###.###.###-##"
+                          customInput={Input}
+                          placeholder="000.000.000-00"
+                          onValueChange={(values) => {
+                            field.onChange(values.value);
+                          }}
+                          value={field.value}
+                          onBlur={field.onBlur}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Celular</FormLabel>
+                      <FormControl>
+                        <PatternFormat
+                          format="(##) #####-####"
+                          customInput={Input}
+                          placeholder="(00) 00000-0000"
+                          onValueChange={(values) => {
+                            field.onChange(values.value);
+                          }}
+                          value={field.value}
+                          onBlur={field.onBlur}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <FormField
+                control={form.control}
+                name="zipCode"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>CEP</FormLabel>
+                    <FormControl>
+                      <PatternFormat
+                        format="#####-###"
+                        customInput={Input}
+                        placeholder="00000-000"
+                        onValueChange={(values) => {
+                          field.onChange(values.value);
+                        }}
+                        value={field.value}
+                        onBlur={field.onBlur}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="address"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Endereço</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Rua, Avenida, etc." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                <FormField
+                  control={form.control}
+                  name="number"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Número</FormLabel>
+                      <FormControl>
+                        <Input placeholder="123" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="complement"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Complemento (Opcional)</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Apto 101" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+                <FormField
+                  control={form.control}
+                  name="neighborhood"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Bairro</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Bairro" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="city"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Cidade</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Cidade" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="state"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Estado</FormLabel>
+                      <FormControl>
+                        <Input placeholder="UF" maxLength={2} {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <Button type="submit" className="w-full">
+                Continuar com o pagamento
+              </Button>
+            </form>
+          </Form>
+        )}
       </CardContent>
     </Card>
   );
