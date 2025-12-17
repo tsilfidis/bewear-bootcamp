61f76b82a39378ac65705e20d7c27cc9f1bedca8
Panagiotis Alexandro Tsilfidis <tsilfidis@gmail.com>
2025-12-17 05:34:47 -0400

    fix: ensure order is created with correct id

--- src/app/cart/confirmation/components/finish-order-button.tsx
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -2,6 +2,7 @@
 
 import { Loader2 } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
@@ -9,15 +10,18 @@ import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogTitle } f
 import { useFinishOrder } from "@/hooks/mutations/use-finish-order";
 
 const FinishOrderButton = () => {
-    const [successDialogIsOpen, setSuccessDialogIsOpen] = useState(true);
+    const [successDialogIsOpen, setSuccessDialogIsOpen] = useState(false);
     const finishOrderMutation = useFinishOrder();
-    
+    const handleFinishOrder = () => {
+        finishOrderMutation.mutate();
+        setSuccessDialogIsOpen(true);
+    }
     return (
         <>
         <Button 
         className="w-full rounded-full" 
         size="lg" 
-        onClick={() => finishOrderMutation.mutate()}
+        onClick={handleFinishOrder}
         disabled={finishOrderMutation.isPending}
         >
             {finishOrderMutation.isPending ? (
@@ -42,7 +46,9 @@ const FinishOrderButton = () => {
                 <DialogDescription className="font-medium">Seu pedido foi efetuado com sucesso. Você pode acompanhar o status na seção de “Meus Pedidos”.</DialogDescription>
 
                 <DialogFooter>
-                    <Button className="rounded-full" variant="ghost" size="lg">Página inicial</Button>
+                    <Button className="rounded-full" variant="ghost" size="lg" asChild>
+                        <Link href="/">Voltar para a loja</Link>
+                    </Button>
                     <Button className="rounded-full" size="lg">Ver meus pedidos</Button>
                 </DialogFooter>
             </DialogContent>
