7f9cf02c7875577da6c20aeeebb39370ab0692b7
Panagiotis Alexandro Tsilfidis <tsilfidis@gmail.com>
2025-12-17 10:56:39 -0400

    feat: create action checkout session stripe

--- src/app/cart/confirmation/components/finish-order-button.tsx
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { loadStripe } from "@stripe/stripe-js"
 import { Loader2 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
+import { createCheckoutSession } from "@/actions/create-checkout-session";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogTitle } from "@/components/ui/dialog";
 import { useFinishOrder } from "@/hooks/mutations/use-finish-order";
@@ -12,8 +14,21 @@ import { useFinishOrder } from "@/hooks/mutations/use-finish-order";
 const FinishOrderButton = () => {
     const [successDialogIsOpen, setSuccessDialogIsOpen] = useState(false);
     const finishOrderMutation = useFinishOrder();
-    const handleFinishOrder = () => {
-        finishOrderMutation.mutate();
+    const handleFinishOrder = async () => {
+        if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+            throw new Error("Stripe publishable key not set");
+        }
+        const { orderId } = await finishOrderMutation.mutateAsync();
+        const checkoutSession = await createCheckoutSession({
+            orderId,
+        });
+        const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+        if (!stripe) {
+            throw new Error("Failed to load Stripe");
+        }
+        await stripe.redirectToCheckout({
+            sessionId: checkoutSession.id,
+        })
         setSuccessDialogIsOpen(true);
     }
     return (
